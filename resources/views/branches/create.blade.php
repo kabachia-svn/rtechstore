7c2860c5bc200a79ff76d3f906e19df6bbab06f3
root <root@localhost.localdomain>
2019-07-12 16:26:06 +0300

    initial Branch View

---
+++ resources/views/branches/create.blade.php
@@ -0,0 +1,27 @@
+@extends('base')
+
+@section('main')
+<div class="row">
+    <div class="col-sm-8 offset-sm-2">
+        <h1 class="display-3">Add a branch</h1>
+        <div>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div><br />
+             @endif
+             <form method="post" action="{{  branches.store}}">
+                 @csrf
+                 <div class="form-group">
+                     <label for="branch_id">Branch ID: </label>
+                     <input type="text" class="form-control" name="branch_id"/>
+                 </div>
+             </form>
+        </div>
+    </div> 
+</div>
+@endsection
