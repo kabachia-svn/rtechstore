d9002f117c06a7adbf6b09ed8bfc8ada85d32192
root <root@localhost.localdomain>
2019-07-13 23:21:12 +0300

    Changed table definition for hq and branch and renamed migrations in the order they should run

---
+++ database/migrations/2019_07_12_00007_create_deliveries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->bigIncrements('delivery_id');
+            $table->timestamp('delivery_date');
+            $table->unsignedBigInteger('supplier_id');
+        });
+
+        Schema::table('deliveries', function (Blueprint $table) {
+            $table->foreign('supplier_id')->references('supplier_id')->on('suppliers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
