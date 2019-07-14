99b635a1c764296b58f9284a3bd653d254ae8265
root <root@localhost.localdomain>
2019-07-14 17:22:44 +0300

    Liftoff for Order CRUD

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $primaryKey = 'order_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'order_id',
+        'date',
+        'headquarters_id',
+    ];
+ 
 }
